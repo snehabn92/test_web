d7baeac1ac778cd364fc32c3e876aee4e224a180
Sneha <sbn@imvu.com>
2025-03-06 11:05:34 +0530

    update build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [vue()],
     server: {
-        host: '0.0.0.0', // This binds the server to all IP addresses, including your public IP
-        port: 8080, // You can set any port you'd like
-    }
+        // host: '0.0.0.0', // This binds the server to all IP addresses, including your public IP
+        port: 4000, // You can set any port you'd like
+        public: '10.63.75.106',
+    },
+    build: {
+        target: 'esnext', // or 'es2020'
+      }
 });
+
